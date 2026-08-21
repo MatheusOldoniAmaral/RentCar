97856c61f529466a3463c1908d48949f92cd956c
Matheus Oldoni Amaral <matheusoldoniamaral2003@gmail.com>
2026-08-21 00:33:21 -0300

    feat: implementa classe Locadora

--- src/com/rentcar/service/Locadora.java
+++ src/com/rentcar/service/Locadora.java
@@ -1,4 +1,28 @@
 package com.rentcar.service;
 
+import com.rentcar.model.Cliente;
+import com.rentcar.model.Locacao;
+import com.rentcar.model.Veiculo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Locadora {
+
+    private List<Veiculo> veiculos = new ArrayList<>();
+    private List<Cliente> clientes = new ArrayList<>();
+    private List<Locacao> locacoes = new ArrayList<>();
+
+    private static final int LIMITE_VEICULOS = 10;
+
+    public void cadastrarVeiculo(Veiculo veiculo) {
+        if (veiculos.size() >= LIMITE_VEICULOS) {
+            throw new IllegalStateException("Limite de veículos cadastrados atingido.");
+        }
+        veiculos.add(veiculo);
+    }
+
+    public void cadastrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
 }
