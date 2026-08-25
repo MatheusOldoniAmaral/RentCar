3596a34457ad94cd661bf1a480e8c1d94733f1ea
Matheus Oldoni Amaral <matheusoldoniamaral2003@gmail.com>
2026-08-24 23:09:02 -0300

    feat: implementa cadastro de veiculo e cliente na Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,157 @@
+import com.rentcar.model.*;
+import com.rentcar.service.Locadora;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Locadora locadora = new Locadora();
 
+        int escolhaMenu = -1;
+
+        while (escolhaMenu != 0) {
+            System.out.println("---------- MENU ----------");
+            System.out.println("1. Cadastrar veículo.");
+            System.out.println("2. Cadastrar cliente.");
+            System.out.println("3. Registrar locação.");
+            System.out.println("4. Finalizar locação.");
+            System.out.println("5. Listar veículos.");
+            System.out.println("6. Ver histórico de locações de um cliente.");
+            System.out.println("7. Ver total gasto por um cliente.");
+            System.out.println("0. Sair.");
+            escolhaMenu = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (escolhaMenu) {
+                case 1:
+                    System.out.println("---------- CADASTRO DE VEÍCULOS ----------");
+                    System.out.println("Escolha categoria do carro que irá cadastrar: ");
+                    System.out.println("1 - Hatch");
+                    System.out.println("2 - Sedan");
+                    System.out.println("3 - SUV");
+                    System.out.println("4 - Utiliário");
+                    System.out.println("5 - Voltar ao menu principal");
+                    int escolhaSubMenu = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (escolhaSubMenu == 5) {
+                        break;
+                    }
+
+                    System.out.println("Digite a marca: ");
+                    String marca = scanner.nextLine();
+
+                    System.out.println("Digite o modelo: ");
+                    String modelo = scanner.nextLine();
+
+                    System.out.println("Digite a placa: ");
+                    String placa = scanner.nextLine();
+
+                    System.out.println("Digite o valor da diária: ");
+                    double valorDiaria = scanner.nextDouble();
+
+                    System.out.println("Digite o ano de fabricação: ");
+                    int anoFabricacao = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (escolhaSubMenu) {
+                        case 1:
+                            Veiculo veiculoHatchCriado = new Hatch(marca, modelo, placa, valorDiaria, anoFabricacao);
+                            locadora.cadastrarVeiculo(veiculoHatchCriado);
+                            break;
+                        case 2:
+                            Veiculo veiculoSedanCriado = new Sedan(marca, modelo, placa, valorDiaria, anoFabricacao);
+                            locadora.cadastrarVeiculo(veiculoSedanCriado);
+                            break;
+                        case 3:
+                            System.out.println("Digite o valor da taxa porte: ");
+                            double taxaPorte = scanner.nextDouble();
+                            scanner.nextLine();
+
+                            Veiculo veiculoSuvCriado = new Suv(marca, modelo, placa, valorDiaria, anoFabricacao, taxaPorte);
+                            locadora.cadastrarVeiculo(veiculoSuvCriado);
+                            break;
+                        case 4:
+                            System.out.println("Digite a capacidade de carga do veículo (em kg): ");
+                            double capacidadeCargaKg = scanner.nextDouble();
+                            scanner.nextLine();
+
+                            System.out.println("Digite o valor cobrado por kg de capacidade (R$): ");
+                            double valorPorKg = scanner.nextDouble();
+                            scanner.nextLine();
+
+                            Veiculo veiculoUtilitarioCriado = new Utilitario(marca, modelo, placa, valorDiaria, anoFabricacao, valorPorKg, capacidadeCargaKg);
+                            locadora.cadastrarVeiculo(veiculoUtilitarioCriado);
+                            break;
+                        default:
+                            System.out.println("OPÇÃO INVÁLIDA!");
+                            break;
+                    }
+                    break;
+                case 2:
+                    System.out.println("---------- CADASTRO DE CLIENTES ---------");
+
+                    System.out.println("Digite o nome: ");
+                    String nome = scanner.nextLine();
+
+                    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+                    System.out.println("Digite a data de nascimento (dd/mm/yyyy): ");
+                    String textoData = scanner.nextLine();
+                    LocalDate dataNascimento = LocalDate.parse(textoData, formato);
+
+                    System.out.println("Digite o CPF: ");
+                    long cpf = scanner.nextLong();
+
+                    System.out.println("Digite o número da CNH: ");
+                    long numeroCnh = scanner.nextLong();
+                    scanner.nextLine();
+
+                    System.out.println("Digite a categoria da CNH: ");
+                    String categoriaCnh = scanner.nextLine();
+
+                    System.out.println("Digite a data de validade da CNH: ");
+                    String textoDataValidadeCnh = scanner.nextLine();
+                    LocalDate dataValidadeCnh = LocalDate.parse(textoDataValidadeCnh, formato);
+
+                    System.out.println("Digite a data de emissão da CNH: ");
+                    String textoDataEmissaoCnh = scanner.nextLine();
+                    LocalDate dataEmissaoCnh = LocalDate.parse(textoDataEmissaoCnh, formato);
+
+                    System.out.println("Digite o telefone para contato: ");
+                    long telefone = scanner.nextLong();
+                    scanner.nextLine();
+
+                    System.out.println("Digite o email: ");
+                    String email = scanner.nextLine();
+
+                    System.out.println("Digite o endereço: ");
+                    String endereco = scanner.nextLine();
+
+                    Cliente novoCliente = new Cliente(nome, dataNascimento, cpf, numeroCnh, categoriaCnh, dataValidadeCnh, dataEmissaoCnh, telefone, email, endereco);
+                    locadora.cadastrarCliente(novoCliente);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 0:
+                    System.out.println("Encerrando o sistema...");
+                    break;
+                default:
+                    System.out.println("OPÇÃO INVÁLIDA!");
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
